a1b37582e7457c88190efb5d7bf23948c9aae636
Ipaiva92 <ipaiva92@outlook.com>
2023-03-31 16:58:44 -0300

    adjusting

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,39 @@
+import { SetStateAction, useState } from "react";
+
+function Home () {
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleChangeName = (event: { target: { value: SetStateAction<string>; }; }) => {
+    setName(event.target.value);
+  };
+
+  const handleChangePassword = (event: { target: { value: SetStateAction<string>; }; }) => {
+    setPassword(event.target.value);
+  };
+
+  return (
+    <div>
+    <input
+      type="text"
+      id="message"
+      name="message"
+      onChange={handleChangeName}
+    />
+
+    <input
+      type="text"
+      id="message"
+      name="message"
+      onChange={handleChangePassword}
+    />
+
+    <h2>Name: {name}</h2>
+
+    <h2>Password: {password}</h2>
+
+  </div>
+  )
+}
+
+export default Home
